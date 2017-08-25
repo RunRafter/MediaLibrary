cd8631336df5a6fcfcda48d31d403b26ce2c08d7
Tony Cai <tcai534@gmail.com>
2017-08-24 22:15:31 -0700

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="https://www.linkedin.com/in/tony-cai/">LinkedIn</a></li>
+				<li><a href="https://github.com/RunRafter">GitHub</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
